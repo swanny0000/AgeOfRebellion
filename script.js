6e053ac709314a83c16507291b9b4050f9acb5ca
Matt Swanhorst <mattswanhorst@gmail.com>
2024-02-19 00:51:40 -0600

    clean up bad files

--- script.js
+++ script.js
@@ -10,14 +10,52 @@ function loadCharacter(character) {
   resetSkillList(COMBAT_SKILL_LIST);
   resetSkillList(KNOWLEDGE_SKILL_LIST);
   resetSkillList(CUSTOM_SKILL_LIST);
-  loadChar(character);
+  if (character == "new") {
+    unlockPage()
+  } else {
+    loadChar(character);
+    lockPage()
+  }
 }
+
+function unlockPage() {}
+function lockPage() {}
+
 function loadChar(character) {
-  var filename = "./characters/" + character + ".json";
-  fetch(filename)
-    .then(results => results.json())
-    .then(console.log)
+  var url = "https://raw.githubusercontent.com/swanny0000/AgeOfRebellion/master/characters/" + character + ".json";
+  fetch(url)
+    .then(res => res.json())
+    .then(out =>
+      updateFromJson(out))
+    .catch(err => { throw err });
 }
+function updateFromJson(jsonData) {
+  loadCharacterInfo(jsonData);
+  loadCharacteristics(jsonData);
+  loadCareerSkills(jsonData);
+  loadSkills(jsonData);
+  loadTalents(jsonData);
+  loadGear(jsonData);
+}
+
+function loadCharacterInfo(data) {}
+function loadCharacteristics(data) {
+  for (var i = 0, size = data.characteristics.length; i < size; i++) {
+    updateCharacteristic(data.career_skills[i].element, data.career_skills[i].value)
+  }
+}
+function loadCareerSkills(data) {
+  for (var i = 0, size = data.career_skills.length; i < size; i++) {
+    activateCareerSkill(data.career_skills[i].skill);
+  }
+}
+function loadSkills(data) {
+  for (var i = 0, size = data.skills.length; i < size; i++) {
+    setRank(data.skills[i].skill, data.skills[i].rank);
+  }
+}
+function loadTalents(data) {}
+function loadGear(data) {}
 
 
 function activateCareerSkill(skill) {
@@ -43,6 +81,10 @@ function setRank(skill, rank = 0) {
 function resetSkill(skill) {
   setRank(skill, 0);
   deactivateCareerSkill(skill);
+}
+
+function updateCharacteristic(element, newVal) {
+  document.getElementById(element).value = newVal;
 }
 
 function resetCharacteristicList() {
